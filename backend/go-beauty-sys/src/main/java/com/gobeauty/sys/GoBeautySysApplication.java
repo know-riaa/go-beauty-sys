bcac8f3ece4cefeed37d9af4e0bb678c8fc134df
Ria <knowriaa@gmail.com>
2026-06-11 18:06:15 -0400

    Set up backend and connected with postgres

---
+++ backend/go-beauty-sys/src/main/java/com/gobeauty/sys/GoBeautySysApplication.java
@@ -0,0 +1,13 @@
+package com.gobeauty.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBeautySysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoBeautySysApplication.class, args);
+    }
+
+}
---
+++ backend/go-beauty-sys/src/test/java/com/gobeauty/sys/GoBeautySysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobeauty.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoBeautySysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
